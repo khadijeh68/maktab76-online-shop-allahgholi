476768f9c26dc3b9f5f3fa3755be0915672f39ca
khadije <khadijeh.allahgholi@gmail.com>
2022-10-11 21:14:29 +0330

    URL:product card.module.css added

--- src/components/product/ProductCard.js
+++ src/components/product/ProductCard.js
@@ -1,41 +1,18 @@
 import { Button, Card } from "react-bootstrap";
-import { makeStyles } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import { BASE_URL } from "../../config/api";
 import { digitsEnToFa } from "@persian-tools/persian-tools";
-
-const useStyles = makeStyles({
-  page: {
-    display: "inline-flex",
-    alignItems: "center",
-    flexWrap: "wrap",
-    flexDirection: "row",
-    justifyContent: "center",
-    padding: "10px",
-    fontFamily: "Vazir-Medium",
-    color: "black",
-    textAlign:"center"
-  },
-  img: {
-    width: "14rem",
-    height: "20rem",
-    alignItems: "center",
-    padding: "10px",
-    backgroundColor: "#ffffff",
-    boxShadow: "0 0 6px rgb(0 0 0 / 20%)",
-  },
-});
+import style from "./ProductCard.module.css"
 
 function ProductCard({productsList,id}) {
-  const classes = useStyles();
 
   return (
     <div>
       {productsList.filter((item) => item.category == id).slice(0, 6).map((product) => {
         return (
           <Link to={`/products/${product.id}`} className="text-decoration-none"  key={product.id}>
-            <div className={classes.page}>
-              <Card className={classes.img}>
+            <div className={style.page}>
+              <Card className={style.img}>
                 <Card.Img
                   style={{ width: "100px" }}
                   variant="top"
